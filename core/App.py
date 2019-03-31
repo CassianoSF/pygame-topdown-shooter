9d92455951a40489ff4bade927991b18e9ab9870
CassianoSf <cassianosf@gmail.com>
2019-03-31 18:06:03 -0300

    Cube

--- core/App.py
+++ core/App.py
@@ -7,137 +7,25 @@ from VertexArray import VertexArray
 from IndexBuffer import IndexBuffer
 from Texture import Texture
 
-VERT_DATA_1 = numpy.array([
-                        -2.0, -2.0,  2.0,
-                         2.0, -2.0,  2.0,
-                        -2.0,  2.0,  2.0,
-                         2.0,  2.0,  2.0,
-                        -2.0,  2.0, -2.0,
-                         2.0,  2.0, -2.0,
-                        -2.0, -2.0, -2.0,
-                         2.0, -2.0, -2.0
-                           #  1.0,  1.0, 1.0,
-                           #  1.0, -1.0, 1.0,
-                           # -1.0, -1.0, 1.0,
-                           # -1.0,  1.0, 1.0,
-
-                           #  1.0,  1.0, -1.0,
-                           #  1.0, -1.0, -1.0,
-                           # -1.0, -1.0, -1.0,
-                           # -1.0,  1.0, -1.0
-                        ],
-                        dtype="float32")
-
-VERT_DATA_2 = numpy.array([0.5, 0.5, 0.0,
-                         0.5, -0.5, 0.0,
-                        -0.5, -0.5, 0.0,
-                        -0.5, 0.5, 0.0],
-                        dtype="float32")
-
-
-TEXTURE_COORD_DATA = numpy.array([
-                                0.0, 0.0, 0.0,
-                                1.0, 0.0, 0.0,
-                                0.0, 1.0, 0.0,
-                                1.0, 1.0, 0.0
-
-
-                                ],
-                                 dtype="float32")
-
-INDICES_1 = numpy.array([
-
-                        0, 1, 2,
-                        2, 1, 3,
-                        2, 3, 4,
-                        4, 3, 5,
-                        4, 5, 6,
-                        6, 5, 7,
-                        6, 7, 0,
-                        0, 7, 1,
-                        1, 7, 3,
-                        3, 7, 5,
-                        6, 0, 4,
-                        4, 0, 2
-
-                         # 0, 1, 3,
-                         # 1, 2, 3,
-                         # 2, 7, 3,
-                         # 2, 7, 6,
-                         # 0, 3, 4,
-                         # 7, 4, 3,
-                         # 0, 1, 5,
-                         # 0, 5, 4,
-                         # 2, 5, 6,
-                         # 1, 5, 2,
-                         # 4, 6, 7,
-                         # 4, 5, 6
-
-
-
-
-                        ],
-                       dtype="int32")
-
-INDICES_2 = numpy.array([0, 1, 3,
-                       1, 2, 3],
-                       dtype="int32")
-
 WINDOW_WIDTH=1280
 WINDOW_HEIGHT=720
 
-g_model = {
-    'translation': [0.0, 0.0, 0.0],
-    'rotation':    [0.0, 0.0, 0.0],
-    'scale':       [1.0, 1.0, 1.0]
-}
-g_view = {
-    'position': [0.0, 0.0, 12.0],
-    'target':   [0.0, 0.0, 0.0],
-    'up':       [0.0, 1.0, 0.0]
-}
-g_projection = {
-    'fovy':   45.0, 
-    'aspect': WINDOW_WIDTH/WINDOW_HEIGHT,
-    'near':   0.1,
-    'far':    200.0,
-    'dtype':  None 
-}
-
 class App:
     def __init__(self):
+        cube = Object("../res/cube.obj")
 
-        cube = Object("../res/suzanne.obj")
-        cube.vertices
-        cube.indices
-        cube.tex_map
-        cube.normals
+        print(cube.vertices)
+        print(cube.tex_map)
+        print(cube.indices)
 
+        self.texture = Texture("../textures/the_floor/the_floor/crate_1.png")
         self.shader = Shader("VertexShader.shader", "FragmentShader.shader")
-
         self.va1 = VertexArray()
         self.vb_box_1 = VertexBuffer(cube.vertices)
         self.va1.add_buffer(0, 3, self.vb_box_1)
         self.vb_texture = VertexBuffer(cube.tex_map)
         self.va1.add_buffer(1, 3, self.vb_texture)
         self.ib = IndexBuffer(cube.indices)
-
-        
-        # self.va1 = VertexArray()
-        # self.vb_box_1 = VertexBuffer(VERT_DATA_1)
-        # self.va1.add_buffer(0, 3, self.vb_box_1)
-        # self.vb_texture = VertexBuffer(TEXTURE_COORD_DATA)
-        # self.va1.add_buffer(1, 3, self.vb_texture)
-        # self.ib = IndexBuffer(INDICES_1)
-
-        # self.va2 = VertexArray()
-        # self.vb_box_2 = VertexBuffer(VERT_DATA_2)
-        # self.va2.add_buffer(0, 3, self.vb_box_2)
-        # self.vb_texture = VertexBuffer(TEXTURE_COORD_DATA)
-        # self.va2.add_buffer(1, 2, self.vb_texture)
-        # self.ib = IndexBuffer(INDICES_2)
-
-        self.texture = Texture("../textures/the_floor/the_floor/crate_1.png")
 
         self.model = {
             'translation': [0.0, 0.0, 0.0],
@@ -157,14 +45,13 @@ class App:
             'dtype':  None 
         }
         self.mvp = self.mount_mvp(self.model, self.view, self.projection)
-        self.key_state = list(map(lambda x :0, list(range(500))))
 
     def mount_mvp(self, model, view, projection):
         trans_matrix = numpy.transpose(pyrr.matrix44.create_from_translation(model['translation']))
         rot_matrix_x = numpy.transpose(pyrr.matrix44.create_from_x_rotation(model['rotation'][0]))
         rot_matrix_y = numpy.transpose(pyrr.matrix44.create_from_y_rotation(model['rotation'][1]))
         rot_matrix_z = numpy.transpose(pyrr.matrix44.create_from_z_rotation(model['rotation'][2]))
-        rot_matrix = numpy.matmul(numpy.matmul(rot_matrix_x, rot_matrix_y),rot_matrix_z)
+        rot_matrix   = numpy.matmul(numpy.matmul(rot_matrix_x, rot_matrix_y),rot_matrix_z)
         scale_matrix = numpy.transpose(pyrr.matrix44.create_from_scale(model['scale'] ))
         model_matrix = numpy.matmul(numpy.matmul(trans_matrix,rot_matrix),scale_matrix)
 
@@ -206,18 +93,12 @@ class App:
 
         self.shader.add_uniform_1i("the_texture", 0)
 
-        
-        self.move()
-        self.mvp = self.mount_mvp(self.model, self.view, self.projection)
-
+        self.mvp = self.motion(self.model, self.view, self.projection)
         self.shader.add_uniform_matrix_4f("mvp", self.mvp)
         self.draw(self.va1, self.ib, self.texture, self.shader)
 
-        # self.shader.add_uniform_matrix_4f("mvp", self.mvp)
-        # self.draw(self.va2, self.ib, self.texture, self.shader)
 
-
-    def move(self):
+    def motion(self, model, view, projection):
         pressed = pygame.key.get_pressed()
         if pressed[pygame.K_UP]:
             self.model['rotation'][0] -= 0.03
@@ -235,6 +116,7 @@ class App:
             self.view['position'][2] -= 0.1
         if pressed[pygame.K_s]:
             self.view['position'][2] += 0.1
+        return self.mount_mvp(model, view, projection)
 
     def handle_event(self, event):
         if event.type == pygame.QUIT:
--- core/Object.py
+++ core/Object.py
@@ -8,12 +8,12 @@ class Object():
         self.normals = []
 
         try:
-            f = open(fileName)
+            file = open(fileName)
             temp_vertices = []
             temp_tex_map = []
             temp_normals = []
 
-            for index, line in enumerate(f):
+            for line in file:
                 if line[:2] == "v ":
                     temp_vertices.append(
                         list(map(float, line.replace("v ", "").replace("\n", "").split(" ")))
@@ -31,20 +31,9 @@ class Object():
                     for triangle in face:
                         v_index, t_index, n_index = triangle.split("/")
 
-                        if v_index == '':
-                            v_index = 0
-                        else:
-                            v_index = int(v_index)-1
-
-                        if t_index == '':
-                            t_index = 0
-                        else:
-                            t_index = int(t_index)-1
-
-                        if n_index == '':
-                            n_index = 0
-                        else:
-                            n_index = int(n_index)-1
+                        v_index = int(v_index or 1) -1
+                        t_index = int(t_index or 1) -1
+                        n_index = int(n_index or 1) -1
 
                         if len(temp_vertices) > t_index:
                             self.vertices.append(temp_vertices[v_index])
@@ -55,10 +44,10 @@ class Object():
                         if len(temp_normals) > n_index:
                             self.normals.append(temp_normals[n_index])
 
-                        self.indices.append(index)
+                        self.indices.append(len(self.indices))
 
 
-            f.close()
+            file.close()
             self.vertices = numpy.array(self.vertices, dtype="float32").flatten()
             self.tex_map = numpy.array(self.tex_map, dtype="float32").flatten()
             self.normals = numpy.array(self.normals, dtype="float32").flatten()
